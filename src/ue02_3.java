0028f0f206f9759d309e8fc87ad024a0e669b9eb
Theresa <s2410238013@students.fh-hagenberg.at>
2024-10-28 14:31:29 +0100

    Übung 3

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,25 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        double radius = 4.0;
+        double centerX = 0.0;
+        double centerY = 0.0;
+
+        Out.println("Geben Sie die x-Koordinate ein.");
+        double x = In.readDouble();
+        Out.println("Geben Sie die y-Koordinate ein.");
+        double y = In.readDouble();
+
+        double distanceFromCenter =  Math.sqrt((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY));
+       
+        if (distanceFromCenter <= radius) {
+            System.out.println("Der Punkt liegt in der grünen Fläche.");
+        } else {
+            System.out.println("Der Punkt liegt außerhalb der grünen Fläche.");
+        }
+ 
     }
 }
