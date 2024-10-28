7dc7c078c0e8425761d056eda336be8fb277d39b
Theresa <s2410238013@students.fh-hagenberg.at>
2024-10-28 13:38:41 +0100

    Erste Übung

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,22 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.println("Geben Sie die erste Zahl ein:");
+        int first = In.readInt();
+        Out.println("Geben Sie die zweite Zahl ein:");
+        int second = In.readInt();
+        Out.println("Geben Sie die dritte Zahl ein:");
+        int third = In.readInt();
+
+        if (first >= second && first >= third) {
+            System.out.println("Die größte Zahl ist: " + first);
+        } else if (second >= first && second >= third) {
+            System.out.println("Die größte Zahl ist: " + second);
+        } else {
+            System.out.println("Die größte Zahl ist: " + third);
+        }
     }
 }
+
